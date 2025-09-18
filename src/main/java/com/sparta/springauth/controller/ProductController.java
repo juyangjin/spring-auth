993572d358ac2686d869b05a94392968f4683d67
joyangjin <wndid2008@gmail.com>
2025-09-18 15:03:55 +0900

    데이터 검증하기

--- src/main/java/com/sparta/springauth/controller/ProductController.java
+++ src/main/java/com/sparta/springauth/controller/ProductController.java
@@ -1,12 +1,17 @@
 package com.sparta.springauth.controller;
 
+import com.sparta.springauth.dto.ProductRequestDto;
 import com.sparta.springauth.entity.User;
 import com.sparta.springauth.security.UserDetailsImpl;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/api")
@@ -20,5 +25,13 @@ public class ProductController {
         System.out.println("user.getEmail() = " + user.getEmail());
 
         return "redirect:/";
+    }
+    /**
+     * TODO 관리자용 예외처리 들어가야 하는 부분, 에러 나서 제외됨 예외 페이지도 html로 resources.static에 들어가야함
+     **/
+    @PostMapping("/validation")
+    @ResponseBody
+    public ProductRequestDto testValid(@RequestBody @Valid ProductRequestDto requestDto) {
+        return requestDto;
     }
 }
